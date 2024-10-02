dc3070e6b6f240bed84c3cc907f04e7a24dbedd0
Lemonish <patrikmihelson@gmail.com>
2024-10-03 00:12:12 +0300

    AppUserRepositoryTest done

--- src/test/java/s24/backend/bookstore/repository/AppUserRepositoryTest.java
+++ src/test/java/s24/backend/bookstore/repository/AppUserRepositoryTest.java
@@ -2,16 +2,19 @@ package s24.backend.bookstore.repository;
 
 
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.matchers.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import s24.backend.bookstore.domain.AppUser;
 import s24.backend.bookstore.domain.AppUserRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 @DataJpaTest
+@ActiveProfiles("test")
 public class AppUserRepositoryTest {
     @Autowired
     private AppUserRepository appUserRepository;
@@ -20,6 +23,7 @@ public class AppUserRepositoryTest {
     public void testCreateUser() {
         AppUser appUser = new AppUser("name", "password", "email@example.com", "USER");
         appUserRepository.save(appUser);
+
         assertThat(appUser.getId()).isNotNull();
     }
     
@@ -27,6 +31,7 @@ public class AppUserRepositoryTest {
     public void testDeleteUser() {
         AppUser appUser = new AppUser("name", "password", "email@example.com", "USER");
         appUserRepository.save(appUser);
+
         Long userId = appUser.getId();
         appUserRepository.delete(appUser);
         assertThat(appUserRepository.findById(userId)).isEmpty();
@@ -47,12 +52,33 @@ public class AppUserRepositoryTest {
 
     @Test
     public void testFindUserById() {
+        AppUser appUser = new AppUser("name", "password", "email@example.com", "USER");
+        appUserRepository.save(appUser);
 
+        Long userId = appUser.getId();
+        AppUser foundUser = appUserRepository.findById(userId).orElse(null);
+        assertThat(foundUser).isNotNull();
+        assertThat(foundUser.getUsername()).isEqualTo("name");
     }
 
     @Test
-    public void testFindUserByUsername() {}
+    public void testFindUserByUsername() {
+        AppUser appUser = new AppUser("name", "password", "email@example.com", "USER");
+        appUserRepository.save(appUser);
+
+        AppUser foundUser = appUserRepository.findByUsername("name");
+        assertThat(foundUser).isNotNull();
+        assertThat(foundUser.getEmail()).isEqualTo("email@example.com");
+    }
 
     @Test
-    public void testFindAllUsers() {}
+    public void testFindAllUsers() {
+        AppUser appUser = new AppUser("name", "password", "email@example.com", "USER");
+        AppUser appUser2 = new AppUser("name2", "password2", "email2@example2.com2", "USER");
+        appUserRepository.save(appUser);
+        appUserRepository.save(appUser2);
+
+        List<AppUser> users = appUserRepository.findAll();
+        assertThat(users).hasSize(2);
+    }
 }
